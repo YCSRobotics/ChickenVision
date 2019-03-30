815db94df69ecc18bbdeef58db7d2b1d744f7f0f
Dalton Smith <gamefollower26@gmail.com>
2019-03-29 22:15:59 -0400

    Revert "default value of 0 of distance, prevent code crash cuz divide by 0"
    This reverts commit e8b3d50e5d1614d34f9a993f0996c5f6699a947e.

--- ChickenVision.py
+++ ChickenVision.py
@@ -347,12 +347,7 @@ def findTape(contours, image, centerX, centerY):
 
                 # Angle from center of camera to target (what you should pass into gyro)
                 yawToTarget = calculateYaw(centerOfTarget, centerX, H_FOCAL_LENGTH)
-                
-                distanceToTarget = 0
-                try:
-                    distanceToTarget = (6/(math.tan(math.radians(targetWidth * degPerPixel))))*2.5
-                except:
-                    pass
+                distanceToTarget = (6/(math.tan(math.radians(targetWidth * degPerPixel))))*2.5
                 
                 # Make sure no duplicates, then append
                 if not targets:
